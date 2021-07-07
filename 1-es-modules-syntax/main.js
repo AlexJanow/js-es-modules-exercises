374a94bfe7ead2395b1cab5d0a4bc8dda2d56261
AlexJanow <85886307+AlexJanow@users.noreply.github.com>
2021-07-07 12:31:17 +0200

    exercise done

--- 1-es-modules-syntax/main.js
+++ 1-es-modules-syntax/main.js
@@ -1,18 +1,5 @@
-function sum(a, b) {
-  return a + b;
-}
-
-function substract(a, b) {
-  return a - b;
-}
-
-function multiply(a, b) {
-  return a * b;
-}
-
-function divide(a, b) {
-  return a / b;
-}
+import sum from "./lib/math.js";
+import { substract, divide, multiply } from "./lib/math.js";
 
 console.log(sum(4, 5));
 console.log(substract(8, 4));
